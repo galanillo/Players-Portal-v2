058e1031290430157766a02bc12299f4f52467e6
Alejandro Galan <alexandrillo8899@gmail.com>
2022-03-15 18:10:40 +0100

    Firebase v2

--- js/app.js
+++ js/app.js
@@ -1,8 +1,12 @@
-import { initializeApp } from 'firebase/app';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// TODO: Replace the following with your app's Firebase project configuration
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  //...
   apiKey: "AIzaSyBtGvQ5KDH8EgnWWJM169BriyXseB8WMCY",
   authDomain: "player-s-portal.firebaseapp.com",
   projectId: "player-s-portal",
@@ -10,19 +14,8 @@ const firebaseConfig = {
   messagingSenderId: "998038850048",
   appId: "1:998038850048:web:ba87b0b3433569c4b126e5",
   measurementId: "G-F5YXJTXNGW"
-
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
-// Get a list of cities from your database
-
-async function getCities(db) {
-
-  const citiesCol = collection(db, 'cities');
-  const citySnapshot = await getDocs(citiesCol);
-  const cityList = citySnapshot.docs.map(doc => doc.data());
-  return cityList;
-  
-}
+const analytics = getAnalytics(app);
